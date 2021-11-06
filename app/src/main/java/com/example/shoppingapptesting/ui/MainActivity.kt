54a83ad4a3126c6bbb7f6682fb97c71d9d398466
Chuei-Zuo Chen <louisa0103a@gmail.com>
2021-11-06 22:35:29 +0800

    Initial commit

---
+++ app/src/main/java/com/example/shoppingapptesting/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shoppingapptesting.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shoppingapptesting.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
