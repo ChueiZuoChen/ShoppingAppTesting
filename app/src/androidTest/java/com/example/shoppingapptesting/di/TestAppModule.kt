b2c5eb324e32de47f6d5822929e0ae5a4bb8ab26
Chuei-Zuo Chen <louisa0103a@gmail.com>
2021-11-13 15:14:07 +0800

    Hilt inject test -2

--- app/src/androidTest/java/com/example/shoppingapptesting/di/TestAppModule.kt
+++ app/src/androidTest/java/com/example/shoppingapptesting/di/TestAppModule.kt
@@ -14,6 +14,12 @@ import javax.inject.Named
 @InstallIn(SingletonComponent::class)
 object TestAppModule {
 
+    /**
+     * 1. DAO測試時我們使用Room的inMemoryDatabaseBuilder()資料庫功能，
+     * 這樣可以跟App原本的資料分開，不用怕測試影響到原資料，之後也不用清除測試資料。
+     * 2. 透過applicationProvider可以得到context
+     * */
+
     @Provides
     @Named("test_db")
     fun provideInMemoryDb(@ApplicationContext context: Context) =
